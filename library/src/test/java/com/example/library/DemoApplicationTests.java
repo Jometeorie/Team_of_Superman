f3d64e05e176b8af69bc93f28cfba49139842609
frozenlalala <1252742119@qq.com>
2020-03-16 22:55:54 +0800

    New a SpringBoot Project

---
+++ library/src/test/java/com/example/library/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
