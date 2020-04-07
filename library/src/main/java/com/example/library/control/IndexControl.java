896069aea70882c69b81b4e18a75cc897929a340
frozenlalala <1252742119@qq.com>
2020-04-07 14:22:34 +0800

    Modify FrontEnd and Add search method

--- library/src/main/java/com/example/library/control/IndexControl.java
+++ library/src/main/java/com/example/library/control/IndexControl.java
@@ -1,29 +1,18 @@
 package com.example.library.control;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-import com.example.library.database.src.team.library.demo.*;
 
-import javax.servlet.http.HttpSession;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-// This is just for test.
 @RestController
 public class IndexControl
 {
-    @RequestMapping(value = "/index")
-    public ModelAndView test(ModelAndView mv, HttpServletRequest request, HttpServletResponse response) {
-        mv.setViewName("/index");
-        // mv.addObject("title", "啦啦啦");
-
-        String id = new String("123");
-        String passwd = new String("1234");
-        boolean flag =new Reader().ReaderLogin(id,passwd);
-
-        HttpSession session = request.getSession();
-        System.out.println(session.getAttribute("username"));
-        mv.addObject("title", flag);
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView test(ModelAndView mv) {
+        mv.setViewName("/MainPage");
         return mv;
     }
+
+    // @RequestMapping(value = "/Mainpage", method = RequestMethod.POST HttpServletRequest request, HttpServletResponse response)
+
 }
