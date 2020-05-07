6392e5b6f312045be5ebf9b22a8b2f8784bf8b75
frozenlalala <1252742119@qq.com>
2020-05-07 23:10:03 +0800

    Update LibrarianFormControl and SearchPageControl

---
+++ library/src/main/java/com/example/library/control/LibrarianFormControl.java
@@ -0,0 +1,35 @@
+package com.example.library.control;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import com.example.library.database.src.team.library.demo.*;
+
+// import javax.servlet.http.HttpSession;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+public class LibrarianFormControl {
+    @RequestMapping(value = "/LibrarianForm", method = RequestMethod.GET)
+    public ModelAndView getLibrarianForm(ModelAndView mv) {
+
+        mv.setViewName("/LibrarianForm");
+        return mv;
+    }
+
+    @RequestMapping(value = "/LibrarianForm", method = RequestMethod.POST)
+    public ModelAndView postBookManagement(ModelAndView mv, HttpServletRequest request, HttpServletResponse response)  throws IOException {
+        // 页眉Logo按钮
+        if (request.getParameter("mainpage") != null) {
+            response.sendRedirect("MainPage");
+            return mv;
+        }
+
+        return mv;
+    }
+}
