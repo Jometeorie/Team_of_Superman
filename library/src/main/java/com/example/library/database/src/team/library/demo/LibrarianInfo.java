b76896eabefc6872e1bf236fadca57e2c52d0c1d
boringfish <1586602728@qq.com>
2020-06-09 17:20:48 +0800

    showalllibrarian

---
+++ library/src/main/java/com/example/library/database/src/team/library/demo/LibrarianInfo.java
@@ -0,0 +1,24 @@
+package com.example.library.database.src.team.library.demo;
+
+public class LibrarianInfo {
+    public String libr_id;
+    public String libr_name;
+    public String password;
+    public int state;
+
+    public void setLibr_id(String libr_id) {
+        this.libr_id = libr_id;
+    }
+
+    public void setLibr_name(String libr_name) {
+        this.libr_name = libr_name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+}
