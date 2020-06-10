88669877b8e88dde8324ec4f083f41f428133059
unknown <sp15596167563@163.com>
2020-06-11 00:09:31 +0800

    图书管理功能完善“

--- library/src/main/java/com/example/library/control/BookManagementControl.java
+++ library/src/main/java/com/example/library/control/BookManagementControl.java
@@ -7,8 +7,10 @@ import com.example.library.database.src.team.library.demo.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
@@ -27,20 +29,26 @@ public class BookManagementControl {
 
 
     // 修改书籍信息
-    @RequestMapping(value = "BookManagement/Modify/{bookID}/{bookName}/{author}/{location}/{price}/{category}/{state}")
+    @RequestMapping(value = "BookManagement/Modify/{bookID}/{bookName}/{author}/{location}/{price}/{category}")
     @ResponseBody
     public ModelAndView changemessage(@PathVariable("bookID") String bookID, @PathVariable ("bookName") String bookName, 
-    @PathVariable("author") String author, @PathVariable("location") String location, @PathVariable("price") String price, 
-    @PathVariable("category") String category, @PathVariable("state") String state, HttpServletRequest request, HttpServletResponse response)  throws IOException {
-    //    Book.Editauthor(author,bookName);
-       return new ModelAndView("redirect:/BookManagement");
+    @PathVariable("author") String author, @PathVariable("location") String location, @PathVariable("price") BigDecimal price,
+    @PathVariable("category") String category, HttpServletRequest request, HttpServletResponse response)  throws IOException {
+        Book.Editauthor(author,bookID);
+        Book.EditBook_Name(bookName,bookID);
+        Book.EditLocation(location,bookID);
+        Book.EditCategory(category,bookID);
+        Book.EditPrice(price,bookID);
+        return new ModelAndView("redirect:/BookManagement");
     }
 
     // 删除书籍信息
     @RequestMapping(value = "BookManagement/Delete/{bookID}")
     @ResponseBody
     public ModelAndView deletemessage(@PathVariable("bookID") String bookID, HttpServletRequest request, HttpServletResponse response)  throws IOException {
-    //    Book.Editauthor(author,bookName);
+       // HttpSession session = request.getSession();
+        //String Libr_id=session.getAttribute("username").toString();
+        //Book.DeleteBook(Libr_id,bookID,bookName);
        return new ModelAndView("redirect:/BookManagement");
     }
 
--- library/src/main/java/com/example/library/database/src/team/library/demo/Book.java
+++ library/src/main/java/com/example/library/database/src/team/library/demo/Book.java
@@ -77,9 +77,9 @@ public class Book{
      * */
     @Test
     public void test2(){
-        System.out.println(new Book().EditLocation("floor4-434-C",2));
-        System.out.println(new Book().EditBook_Name("i want ",2));
-        System.out.println(new Book().EditCategory("math",2));
+        System.out.println(new Book().EditLocation("floor4-434-C","2"));
+        System.out.println(new Book().EditBook_Name("i want ","2"));
+        System.out.println(new Book().EditCategory("math","2"));
 
     }
     /**
@@ -87,7 +87,7 @@ public class Book{
      * 编辑LOCATION
      * */
 
-    public Boolean EditLocation(String location,int book_id){
+    public static Boolean EditLocation(String location,String book_id){
         JdbcTemplate template=new JdbcTemplate(JdbcUtils.getDataSource());
         String sql="update book set LOCATION=? where BOOK_ID=?";
         int count=template.update(sql,location,book_id);
@@ -99,7 +99,7 @@ public class Book{
      * 编辑书籍信息
      * 编辑名称
      * */
-    public Boolean EditBook_Name(String book_name,int book_id){
+    public static Boolean EditBook_Name(String book_name,String book_id){
         JdbcTemplate template=new JdbcTemplate(JdbcUtils.getDataSource());
         String sql="update book set BOOK_NAME=? where BOOK_ID=?";
         int count=template.update(sql,book_name,book_id);
@@ -125,7 +125,7 @@ public class Book{
      * 编辑书籍信息
      * 编辑书籍价格
      * */
-    public Boolean EditPrice(BigDecimal price, String Book_id){
+    public static Boolean EditPrice(BigDecimal price, String Book_id){
         JdbcTemplate template=new JdbcTemplate(JdbcUtils.getDataSource());
         String sql="update book set PRICE=? where BOOK_ID=?";
         int count=template.update(sql,price,Book_id);
@@ -137,7 +137,7 @@ public class Book{
      * 编辑书籍信息
      * 编辑书籍类别
      * */
-    public Boolean EditCategory(String category, int book_id){
+    public static Boolean EditCategory(String category, String book_id){
         JdbcTemplate template=new JdbcTemplate(JdbcUtils.getDataSource());
         String sql="update book set CATEGORY=? where BOOK_ID=?";
         int count=template.update(sql,category,book_id);
@@ -183,7 +183,7 @@ public class Book{
     }
     @Test
     public  void test4(){
-        boolean flag=Book.DeleteBook("17130177001",4,"hhhhh");
+        boolean flag=Book.DeleteBook("17130177001","4","hhhhh");
         System.out.println(flag);
 
     }
@@ -191,7 +191,7 @@ public class Book{
      * 删除书籍，并将删除信息添加到book_deleted
      * */
 
-    public static boolean DeleteBook(String Libr_ID,int Book_ID,String Book_Name) {
+    public static boolean DeleteBook(String Libr_ID,String Book_ID,String Book_Name) {
         Connection con=null;
         PreparedStatement stmt=null;
         ResultSet rs =null;
@@ -199,7 +199,7 @@ public class Book{
             con=JdbcUtils.getConnection();
             String sql="delete from book where BOOK_ID=?";
             stmt=con.prepareStatement(sql);
-            stmt.setInt(1,Book_ID);
+            stmt.setString(1,Book_ID);
             int result=stmt.executeUpdate();
             if (result>0)
                 InsertBookDeleted(Libr_ID,Book_ID,Book_Name);
@@ -212,7 +212,7 @@ public class Book{
         }
         return false;
     }
-    public static boolean InsertBookDeleted(String Libr_ID,int Book_ID,String Book_Name) {
+    public static boolean InsertBookDeleted(String Libr_ID,String Book_ID,String Book_Name) {
         Connection con =null;
         PreparedStatement stmt=null;
         ResultSet rs=null;
@@ -220,7 +220,7 @@ public class Book{
             con=JdbcUtils.getConnection();
             String sql="insert into book_deleted values (?,?,?)";
             stmt=con.prepareStatement(sql);
-            stmt.setInt(1,Book_ID);
+            stmt.setString(1,Book_ID);
             stmt.setString(2,Book_Name);
             stmt.setString(3,Libr_ID);
             stmt.executeUpdate();
